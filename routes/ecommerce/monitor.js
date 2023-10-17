f723e8460c9777ffc294224997151d626f52f78f
lukimme <lukmanhakim8832@gmail.com>
2023-10-17 19:59:26 +0700

    monitot

--- routes/ecommerce/monitor.js
+++ routes/ecommerce/monitor.js
@@ -5,87 +5,75 @@ const moment = require('moment')
 const { format } = require('date-fns');
 const verifyToken = require('../../middleware/jwttoken')
 
-// router.get('/monitorOrder', (req, res) => {
-//     const id_akun = req.params.idAkun
-//     // const startDate = req.query.start_date;//mengambil data mulai dari
-//     // const endDate = req.query.end_date;//mengambil data akhir
-//     // const tglHariIni = req.query.start_date;//mengambil data mulai dari
-//     // const tglSaatIni = new Date();
-//     // // const hari = String(tglSaatIni.getDate()).padStart(2, '0');
-//     // const bulan = String(tglSaatIni.getMonth() + 1).padStart(2, '0');
-//     // const tahun = tglSaatIni.getFullYear();
-
-//     const fotmatTanggal = req.params.forTgl;
-
-//     // console.log(fotmatTanggal);
-
-//     // Mengambil data admin dari database
-//     db('data_order_ecom')
-//     .select(
-//         'data_order_ecom.*',
-//         'bahan_cetak.nama_bahan_cetak',
-//         'mesin_cetak.nama_mesin_cetak',
-//         'akun_ecom.nama_akun_ecom',
-//         'ekspedisi.nama_ekspedisi',
-//         'laminasi.nama_laminasi',
-//         'setting_order.status as Tahap',
-//         db.select('akun.nama_akun').from('akun').whereRaw('akun.id_akun = setting_order.id_akun').as('Nama Penyeting'),
-//         'akun.status_akun as status Desainer',
-//         'akun.foto_akun as foto desainer'
-//     )
-//     .join('bahan_cetak', 'data_order_ecom.id_bahan_cetak', 'bahan_cetak.id_bahan_cetak')
-//     .join('mesin_cetak', 'data_order_ecom.id_mesin_cetak', 'mesin_cetak.id_mesin_cetak')
-//     .join('akun_ecom', 'data_order_ecom.id_akun_ecom', 'akun_ecom.id_akun_ecom')
-//     .join('ekspedisi', 'data_order_ecom.id_ekspedisi', 'ekspedisi.id_ekspedisi')
-//     .join('laminasi', 'data_order_ecom.id_laminasi', 'laminasi.id_laminasi')
-//     .join('setting_order', 'data_order_ecom.id_order_ecom', '=', 'setting_order.id_order')
-//     .then((data) => {
-//         const formattedData = data.map((item) => ({
-//             ...item,
-//             tanggal_order_formatted: format(new Date(item.order_time), "dd MMM yyyy HH:mm"),
-//             tanggal_input_formatted: format(new Date(item.time), "dd MMM yyyy HH:mm"),
-//         }));
-
-//         res.json(formattedData);
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//         res.status(500).json({ error: 'An error occurred' });
-//     });
 
 
-
-
-// });
-
-
-router.get('/monitorOrder', (req, res) => {
-   
-    
-    db('setting_order')
-    .select('data_order_ecom.*', 'setting_order.*', 'bahan_cetak.nama_bahan_cetak')
-    .join('data_order_ecom', 'setting_order.id_order', 'data_order_ecom.id_order_ecom')
+//membuat async arrow function bernama getMonitorData
+const getMonitorData = async (req, res) => {
+    db.select('data_order_ecom.*', 'akun.nama_akun AS nama_desainer','bahan_cetak.nama_bahan_cetak'
+    , 'mesin_cetak.nama_mesin_cetak', 'akun_ecom.nama_akun_ecom'
+    , 'ekspedisi.nama_ekspedisi', 'laminasi.nama_laminasi', 'setting_order.status'
+    ,'setting_order.time_start AS mulai_setting','setting_order.time_finish AS selesai_setting')
+    .from('data_order_ecom')
+    .join('setting_order', 'data_order_ecom.id_order_ecom', '=', 'setting_order.id_order')
     .join('bahan_cetak', 'data_order_ecom.id_bahan_cetak', 'bahan_cetak.id_bahan_cetak')
-    .limit(1)
+    .join('mesin_cetak', 'data_order_ecom.id_mesin_cetak', 'mesin_cetak.id_mesin_cetak')
+    .join('akun_ecom', 'data_order_ecom.id_akun_ecom', 'akun_ecom.id_akun_ecom')
+    .join('ekspedisi', 'data_order_ecom.id_ekspedisi', 'ekspedisi.id_ekspedisi')
+    .join('laminasi', 'data_order_ecom.id_laminasi', 'laminasi.id_laminasi')
+    .leftJoin('akun', 'data_order_ecom.id_akun', '=', 'akun.id_akun')
 
+    //jika id_akun di setting order masih kosong maka kosongkan juga hasilnya
+    .select(db.raw('COALESCE(setting_order.id_akun, "") as id_penyetting'))
+    .orderBy('data_order_ecom.order_time','asc')
     .then((data) => {
-        const formattedData = data.map((item) => ({
-            ...item,
-            tanggal_order_formatted: format(new Date(item.order_time), "dd MMM yyyy HH:mm"),
-            tanggal_input_formatted: format(new Date(item.time), "dd MMM yyyy HH:mm"),
-        }));
 
-        res.json(formattedData);
+        //membuat array berisi id_akun hasil select diatas
+        const idPenyettingValues = data.map(item => item.id_penyetting);
+
+        //melakukan looping select nama akun sesuai array yang telah kita buat tadi (idPenyettingValues)
+        db('akun')
+        .leftJoin('akun as a', 'a.id_akun', 'akun.id_akun')
+        .whereIn('akun.id_akun', idPenyettingValues)
+        .select('akun.id_akun', db.raw('COALESCE(a.nama_akun, "") as nama_akun'))
+        .then((results) => {
+
+          /*
+          melakukan proses penggantian isi array dari id_akun menjadi nama_akun
+          */
+            for (let i = 0; i < idPenyettingValues.length; i++) {
+                if (idPenyettingValues[i] !== '') {
+                  const matchingAccount = results.find(item => item.id_akun === idPenyettingValues[i]);
+                  if (matchingAccount) {
+                    idPenyettingValues[i] = matchingAccount.nama_akun;
+                  } else {
+                    idPenyettingValues[i] = '';
+                  }
+                } else {
+                  idPenyettingValues[i] = '';
+                }
+              }
+
+              /*
+              setelah mempunyai array berisi nama_akun maka buat array baru gabungan dari berisi
+              nama_akun dan array response select diatas
+              */
+
+              const newData = data.map((item, index) => {
+                return { ...item, nama_penyetting: idPenyettingValues[index] };
+              });
+
+              //kirim response 
+              res.json(newData)
+
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+       
     })
-    .catch((error) => {
-        console.log(error);
-        res.status(500).json({ error: 'An error occurred' });
-    });
+}
 
-
-    
-
-});
+router.get('/monitor_order',getMonitorData)
 
 
 
